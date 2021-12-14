19e986dbd1c18084e39735ea1240f97fcd676329
mingyeongmo <mingyeongmp@naver.com>
2021-12-14 16:21:16 +0900

    페이지마다 메타데이터(title) 다르게 표시하기

--- pages/tests/test.js
+++ pages/tests/test.js
@@ -1,9 +1,12 @@
 import Image from 'next/image'
-
+import Head from 'next/head'
 export default function A() {
     return (
         <>
-            <h1>image페이지</h1>
+            <Head>
+                <title>Image페이지</title>
+            </Head>
+            <h1>Image페이지</h1>
             <Image
                 src="/images/profile.jpeg"
                 height={144}
