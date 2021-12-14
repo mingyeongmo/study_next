72ce9085cd8e97f595cea6582987f7fb4e20dcb2
mingyeongmo <mingyeongmp@naver.com>
2021-12-14 12:26:33 +0900

    Link로 페이지 이동하기

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,7 +11,14 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href="/posts/first-posts">
+            <a>this page!</a>
+          </Link>
+          Test{' '}
+          <Link href="/tests/test">
+            <a>test page!</a>
+          </Link>
         </h1>
 
         <p className="description">
