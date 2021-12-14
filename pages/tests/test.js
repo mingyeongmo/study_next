841dea7682bbb81e2f96c01d85d962d1c1679791
mingyeongmo <mingyeongmp@naver.com>
2021-12-14 20:26:12 +0900

    Next 레이아웃 짜기

--- pages/tests/test.js
+++ pages/tests/test.js
@@ -1,18 +1,22 @@
 import Image from 'next/image'
 import Head from 'next/head'
+import Layout from '../components/layout'
 export default function A() {
     return (
         <>
+        <Layout>
             <Head>
                 <title>Image페이지</title>
             </Head>
-            <h1>Image페이지</h1>
-            <Image
-                src="/images/profile.jpeg"
-                height={144}
-                width={144}
-                alt="Your Name"
-            />
+                <h1>Image페이지</h1>
+                <Image
+                    src="/images/profile.jpeg"
+                    height={144}
+                    width={144}
+                    alt="Your Name"
+                />
+        </Layout>
+            
         </>
     )
 }
